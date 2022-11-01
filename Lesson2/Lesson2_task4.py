51cf094d5e62f589200642cad0be1c0dc6ddd272
VladOsadchyi <vladosadcyj340@gmail.come>
2022-11-01 19:00:55 +0200

    Added Lesson2 Homework

---
+++ Lesson2/Lesson2_task4.py
@@ -0,0 +1,12 @@
+print("Hi my dear friend!:3", end="\n")
+print("Welcome to my program that will find the vectorial ofyour number", end="\n")
+print("But you must write only positive numbers and only natural numbers", end="\n")
+print("Let's start", end="\n")
+YourNumber=int(input("Enter your number:"))
+f=1
+while YourNumber>1:
+    f*=YourNumber
+    YourNumber=YourNumber-1
+print(f)
+print("You are great!:3")
+    
