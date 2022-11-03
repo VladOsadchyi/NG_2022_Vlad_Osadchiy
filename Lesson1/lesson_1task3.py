7962a03bd0b78dc284c731306c189e2274a7c9d1
VladOsadchyi <114793229+VladOsadchyi@users.noreply.github.com>
2022-11-03 13:45:12 +0200

    Delete Lesson1 directory

--- Lesson1/lesson_1task3.py
+++
@@ -1,8 +0,0 @@
-print("Hi my dear friend :3", end="\n")
-print("Welcome to my translator(homework) fron seconds to day:hours:minutes:seconds", end="\n")
-print("The name of this program is 'Gogol'", end="\n")
-print("I hope you will like it", end="\n")
-print("So you must write your seconds, without milliseconds", end="\n")
-Yourseconds=int(input("Enter seconds"))
-import datetime
-print(datetime.datetime.fromtimestamp(Yourseconds).strftime('%d:%H:%M:%S'))
