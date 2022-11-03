801d2484626610b477a94de22a35603faeabe3e4
VladOsadchyi <vladosadcyj340@gmail.come>
2022-11-03 15:47:18 +0200

    Added Lesson1

--- Lesson1/Lesson_1task4.py
+++ Lesson1/Lesson_1task4.py
@@ -1,12 +1,20 @@
-print("We should find the discriminant" )
+print("Hi my dear friend!:3")
+print("Welcome to my program!It will find square roots of your equations")
+print("We wil use the discriminant")
+print("Write your three numbers: a, b, c")
+print("It will look like: ax^2+-bx+-c=0")
+print("Write your numbers")
+a=int(input("Enter your a: "))
+b=int(input("Enter your b: "))
+c=int(input("Enter your c: "))
 D=b**2-4*a*c
 print("D=", D)
 print("We should  find x1 and x2", end="\n")
 x1=float 
 x2=float
 if D>=0:
-    x1=(-b-math.sqrt(D))/(2*a)
-    x2=(-b+math.sqrt(D))/(2*a)
+    x1=(-b-(D*0.5))/(2*a)
+    x2=(-b+(D*0.5))/(2*a)
     print("x1= ", x1)
     print("x2= ", x2)
 else:
