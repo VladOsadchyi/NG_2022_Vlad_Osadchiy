53a44cfcb0d9f182c9afa5aa6113c4115bd8f460
VladOsadchyi <vladosadcyj340@gmail.come>
2022-11-06 14:50:31 +0200

    Fixed bugs

--- Lesson2/Lesson2_task3.py
+++ Lesson2/Lesson2_task3.py
@@ -1,5 +1,5 @@
 print("Hello my friend!:3")
-print("Welcome to my program, it will work like unixtime")
+print("Welcome to my program, it will make pyramids")
 print("Firstly write your number")
 Yournumber=int(input("Enter your number: "))
 while Yournumber>0:
