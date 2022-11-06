1254f199b1422d16b2de6d4174ddb3591a44faa8
VladOsadchyi <vladosadcyj340@gmail.come>
2022-11-06 14:42:38 +0200

    Fixed bags

--- Lesson2/Lesson2_task3.py
+++ Lesson2/Lesson2_task3.py
@@ -1,3 +1,6 @@
+print("Hello my friend!:3")
+print("Welcome to my program, it will work like unixtime")
+print("Firstly write your number")
 Yournumber=int(input("Enter your number: "))
 while Yournumber>0:
     for row in range(-Yournumber,0):
